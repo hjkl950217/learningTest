89e02ced2e33bf2557af3c9921273c03d3f57c49
Lynn.X.wang <lynn.X.Wang@newegg.com>
2018-02-23 09:45:58 +0800

    初始化学习项目

---
+++ 时间格式验证/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 时间格式验证
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*参考资料-datetime tostring中自定义的格式资料
+             * https://docs.microsoft.com/zh-cn/dotnet/standard/base-types/custom-date-and-time-format-strings
+             */
+            Console.WriteLine("Task Start\r\n");
+            List<object> outputList = new List<object>( );
+
+            string dt1 = DateTime.Now.ToString( "o" );
+            string dt2 = DateTime.Now.ToString( "yyyy-MM-ddTHH:mm:sszzz" );
+
+
+            //unix时间戳
+            long dt3= DateTimeOffset.UtcNow.ToUnixTimeMilliseconds( );
+            
+
+            //输出
+            outputList.Add( dt1 );
+            outputList.Add( dt2 );
+            outputList.Add( dt3 );
+
+            int index = 1;
+            foreach( var item in outputList )
+            {
+                Console.WriteLine($"第{index++}个");
+                Console.WriteLine(item);
+                Console.WriteLine("-----");
+            }
+
+            Console.WriteLine("----------------------");
+
+            //转换回去
+            outputList.Clear( );
+            index = 1;
+            DateTime dt10 = Convert.ToDateTime( dt1 );
+            DateTime dt20 = Convert.ToDateTime( dt2 );
+            DateTime dt30 = DateTimeOffset.FromUnixTimeMilliseconds( dt3 ).DateTime.AddHours(8);
+
+
+            outputList.Add( dt10 );
+            outputList.Add( dt20 );
+            outputList.Add( dt30 );
+            foreach( var item in outputList )
+            {
+                Console.WriteLine( $"第{index++}个" );
+                Console.WriteLine( item );
+                Console.WriteLine( "-----" );
+            }
+
+            Console.WriteLine("Task End");
+            Console.ReadLine( );
+        }
+
+
+    }
+
+
+
+}
---
+++ 线程ID验证/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace 线程ID验证
+{
+    internal class Program 
+    {
+        public static async Task GetA()
+        {
+            ShowTheadInfo( "GetA" );
+            await Task.Run( () => { ShowTheadInfo( "GetAAA" ); return "A"; } );
+
+        }
+
+        public static Task GetB()
+        {
+           
+            ShowTheadInfo( "GetB" );
+          return  Task.Run( () => { return "B"; } );
+        }
+
+
+
+        private static void Main( string[] args )
+        {
+            Console.WriteLine( "验证开始" );
+
+            Task.Run( async () => { await GetA(); } );
+            Thread.Sleep( 1 * 1000 );
+
+            Task.Run( () => { GetB(); } );
+            Thread.Sleep( 1 * 1000 );
+
+            ShowTheadInfo( "Main" );
+
+            Console.WriteLine( "主代码执行完毕" );
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        #region 获取线程信息
+
+        /// <summary>
+        /// 获取操作系统线程ID
+        /// </summary>
+        /// <param name="thread"></param>
+        /// <returns></returns>
+        public static int GetNativeThreadId( Thread thread )
+        {
+            var f = typeof( Thread ).GetField( "DONT_USE_InternalThread" ,
+                BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance );
+
+            var pInternalThread = (IntPtr)f.GetValue( thread );
+            //var nativeId = Marshal.ReadInt32( pInternalThread , ( IntPtr.Size == 8 ) ? 548 : 348 );
+            var nativeId = Marshal.ReadInt32( pInternalThread , ( IntPtr.Size == 8 ) ? 0x022C : 0x0160 );
+            return nativeId;
+        }
+
+        /// <summary>
+        /// 获取托管线程ID
+        /// </summary>
+        /// <param name="thread"></param>
+        /// <returns></returns>
+        public static int GetManagedThreadId( Thread thread )
+        {
+            return thread.ManagedThreadId;
+        }
+
+        /// <summary>
+        /// 获取托管线程哈希值
+        /// </summary>
+        /// <param name="thread"></param>
+        /// <returns></returns>
+        public static int GetThreadHashCode( Thread thread )
+        {
+            return thread.GetHashCode();
+        }
+
+        /// <summary>
+        /// 显示线程信息
+        /// </summary>
+        public static void ShowTheadInfo( string methodName )
+        {
+            Thread nowThead = Thread.CurrentThread;
+
+            //int sysID = GetNativeThreadId( nowThead );
+            int managedID = GetManagedThreadId( nowThead );
+            int managedHashCode = GetThreadHashCode( nowThead );
+
+            Console.WriteLine( $"方法名:{methodName}" );
+            //Console.WriteLine( $"系统线程ID{sysID}" );
+            Console.WriteLine( $"托管线程ID:{managedID}" );
+            Console.WriteLine( $"托管线程哈希代码:{managedHashCode}" );
+            Console.WriteLine( "--------------------" );
+        }
+
+        #endregion 获取线程信息
+
+    }
+
+}
