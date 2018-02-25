f8628735d96586e8db042feb5a8d4046868c7090
Lynn.X.wang <lynn.X.Wang@newegg.com>
2018-02-25 10:29:42 +0800

    修改端口

--- AutoMapper学习与练习/Program.cs
+++ AutoMapper学习与练习/Program.cs
@@ -20,6 +20,7 @@ namespace AutoMapper学习与练习
         public static IWebHost BuildWebHost( string[ ] args ) =>
             WebHost.CreateDefaultBuilder( args )
                 .UseStartup<Startup>( )
+                .UseUrls("http://*:5000")
                 .Build( );
     }
 }
