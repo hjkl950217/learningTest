2a29d57d85edfefda67b946069f2ecc449c2dcab
lynn <lynn.x.wang@newegg.com>
2018-07-13 17:28:32 +0800

    建造者模式

---
+++ 建造者模式学习/{两种实现}/AoDiBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace 建造者模式
+{
+    /// <summary>
+    /// 具体创建者，具体的车型的创建者，例如：奥迪
+    /// </summary>
+    public sealed class AoDiBuilder : IBuilder
+    {
+        private readonly Car aoDiCar = new Car();
+
+        private void UsePartBase(string partName,string part = null)
+        {
+            if(part == null)
+            {
+                part = $"Default {partName}";
+            }
+
+            this.aoDiCar.Add(partName,part);
+        }
+
+        public IBuilder UseCarDoor(string part = null)
+        {
+            this.UsePartBase("Door",part);
+            return this;
+        }
+
+        public IBuilder UseCarWheel(string part = null)
+        {
+            this.UsePartBase("Wheel",part);
+            return this;
+        }
+
+        public IBuilder UseCarEngine(string part = null)
+        {
+            this.UsePartBase("Engine",part);
+            return this;
+        }
+
+        public IBuilder UseOtherPart(string partName,string part)
+        {
+            this.aoDiCar.Add(partName,part);
+            return this;
+        }
+
+        public Car BuildCar()
+        {
+            Console.WriteLine("汽车开始在组装.......");
+            Console.WriteLine("汽车组装好了");
+            return this.aoDiCar;
+        }
+    }
+}
---
+++ 建造者模式学习/{两种实现}/BuickBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 建造者模式
+{
+
+    /// <summary>
+    /// 具体创建者，具体的车型的创建者，例如：别克
+    /// </summary>
+    public sealed class BuickBuilder : IBuilder
+    {
+        private readonly Car buickCar = new Car();
+
+        public IBuilder UseCarDoor()
+        {
+            this.buickCar.Add("Buick's Door");
+            return this;
+        }
+
+        public IBuilder UseCarWheel()
+        {
+            this.buickCar.Add("Buick's Wheel");
+            return this;
+        }
+
+        public IBuilder UseCarEngine()
+        {
+            this.buickCar.Add("Buick's Engine");
+            return this;
+        }
+
+        public Car BuildCar()
+        {
+            return this.buickCar;
+        }
+    }
+}
