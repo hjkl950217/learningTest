0f26c0052d049becdc6665009998b139474851b5
王学懿 <hjkl950217@live.com>
2018-08-23 21:32:49 +0800

    新加内存缓存的学习

---
+++ 内存缓存学习/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace 内存缓存学习.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    
+
+        public ValuesController()
+        {
+         
+        }
+
+       
+        [HttpGet]
+        public dynamic Get()
+        {
+         
+
+
+            return "";
+        }
+
+    }
+}
