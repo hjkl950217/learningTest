f6a03f61514b2c9397de4dca7b619bb07f6b2093
王学懿 <hjkl950217@live.com>
2018-09-03 23:01:35 +0800

    补充内存缓存demo

--- 内存缓存学习/Controllers/ValuesController.cs
+++ 内存缓存学习/Controllers/ValuesController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Threading;
 
 namespace 内存缓存学习.Controllers
 {
@@ -11,21 +10,52 @@ namespace 内存缓存学习.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-    
+        private IMemoryCache memoryCache;
 
-        public ValuesController()
+        public ValuesController(IMemoryCache memoryCache)
         {
-         
+            this.memoryCache = memoryCache;
         }
 
-       
-        [HttpGet]
+        private readonly string key = "AA";
+
+        [HttpGet("Get")]
         public dynamic Get()
         {
-         
+            var result = this.memoryCache.Get<string>(this.key);
 
+            return new
+            {
+                Time=DateTimeOffset.Now,
+                result=   result
+            };
+        }
 
-            return "";
+        [HttpGet("Add")]
+        public dynamic Add()
+        {
+            //DateTimeOffset expire = this.GetExpire(10);
+            //this.memoryCache.Set(this.key, "ABCD", expire);
+
+            MemoryCacheEntryOptions option = this.GetOption(5);
+            this.memoryCache.Set(this.key, "ABCD", option);
+
+            return "OK";
+        }
+
+        public DateTimeOffset GetExpire(int expireNum)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(DateTimeOffset.UtcNow.ToUnixTimeSeconds() + expireNum);
+        }
+
+        public MemoryCacheEntryOptions GetOption(int expireNum)
+        {//SlidingExpiration
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            options.SlidingExpiration = TimeSpan.FromSeconds(expireNum);//滑动过期时间
+            options.AbsoluteExpirationRelativeToNow= TimeSpan.FromSeconds(expireNum+3);//绝对过期时间
+
+            return options;
         }
 
     }
