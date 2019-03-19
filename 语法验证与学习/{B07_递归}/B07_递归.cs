3450ed5c32aa29f9bc721c25f93e4937d3179342
lynn <lynn.x.wang@newegg.com>
2019-03-19 19:03:32 +0800

    .

--- 语法验证与学习/{B07_递归}/B07_递归.cs
+++ 语法验证与学习/{B07_递归}/B07_递归.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Diagnostics;
 using Verification.Core;
 
 namespace 语法验证与学习
@@ -24,18 +24,32 @@ namespace 语法验证与学习
             int b = this.Recursive(5);
             Console.WriteLine($"递推与递归\t递推{a}\t递归{b}");
 
-            // 2. 结算货币找零有多少种方式
-            int money = 63;
+            #region 2. 结算货币找零 找最少张数
+
+            int money = 10;
             //int[] coins = new int[] { 1, 2, 5, 10, 20, 50, 100 };
-            int[] coins = new int[] { 1, 5, 10, 50 };
-            int result = this.CountChange_Greedy(money, coins);
-            Console.WriteLine($"结算货币找零有多少种方式: {result}种");
+            int[] coins = new int[] { 1, 2, 5 };
+            Console.WriteLine();
+            Console.WriteLine("结算货币找零有多少种方式:");
+            Console.WriteLine("方式\t\t耗时\t计算次数");
 
-            //贪心算法与动态规划的性能对比
+            Stopwatch sp = new Stopwatch();
 
-            int monay_A = 7;
-            int[] coins_A = new int[] { 1, 2, 5 };
-            int result_A = this.CountChange_DynamicPlan(monay_A, coins_A);
+            sp.Start();
+            int result = this.CountChange_Recursive(money, coins);
+            sp.Stop();
+            Console.WriteLine($"递归\t\t{result}种\t用时{sp.ElapsedTicks}毫微秒\t计算{this.RecursiveNum}次");
+
+
+            sp.Restart();
+            int result_A = this.CountChange_DynamicPlan(money, coins);
+            sp.Stop();
+            Console.WriteLine($"动态规划\t{result_A}种\t用时{sp.ElapsedTicks}毫微秒\t计算{this.DynamicPlanNum}次");
+
+            Console.WriteLine();
+            Console.WriteLine("100毫微秒为CPU一个晶振周期，为十亿分之一秒。 ns 纳秒 毫微秒");
+
+            #endregion 2. 结算货币找零 找最少张数
         }
 
         #region 递推与递归
@@ -68,12 +82,22 @@ namespace 语法验证与学习
 
         #region 结算货币找零有多少种方式
 
-        //贪心算法版本
-        private int GreedyNum = 0;
+        //递归版本
+        private int RecursiveNum = 0;
 
-        public int CountChange_Greedy(int money, int[] coins)
+        /// <summary>
+        /// 用递归找： 金额找零时最少需要多少张
+        /// 复杂为度为 coins.Length^money
+        /// </summary>
+        /// <param name="money"></param>
+        /// <param name="coins"></param>
+        /// <returns></returns>
+        public int CountChange_Recursive(int money, int[] coins)
         {
-            this.GreedyNum++;
+            this.RecursiveNum++;
+
+            int min_ret = money;
+
             if (money == 1)
             {
                 return 1;
@@ -83,42 +107,79 @@ namespace 语法验证与学习
                 return 0;
             }
 
-            //第一个：先找出一个，然后把剩余的在所有方式中查找
-            //第二个：所有方式中，跳过第一个，然后找所有的
+            foreach (var c in coins)
+            {
+                if (money > c)
+                {
+                    /*
+                     * 下面这二行代码是程序的出口。
+                     * 跳出的时候一定是money和c只相差一块。 比如 3块钱，金额为2。11块钱 金额为10
+                     *
+                     * 每次进入，都是一次新的循环。min_ret也只是当前进入时，找最少的张数
+                     *
+                     *
+                     */
 
-            return this.CountChange_Greedy(money - coins.First(), coins)
-                + this.CountChange_Greedy(money, coins.Skip(1).ToArray());
+                    int min_count = this.CountChange_Recursive(money - c, coins) + 1;
+                    if (min_count < min_ret)
+                    {
+                        min_ret = min_count;
+                    }
+                }
+            }
+
+            return min_ret;
+
+            ////第一个：先找出一个，然后把剩余的 在所有方式中查找
+            ////第二个：所有方式中，跳过第一个，然后找所有的
+
+            //return this.CountChange_Greedy(money - coins.First(), coins)
+            //    + this.CountChange_Greedy(money, coins.Skip(1).ToArray());
         }
 
         //动态规划版本
         private int DynamicPlanNum = 0;
 
-        public int CountChange_DynamicPlan(int money, int[] coins, int[] ways = null)
+        /// <summary>
+        /// 用动态规划找： 金额找零时最少需要多少张
+        /// 复杂为度为：C^amount C是一个常数，代表机器当前运行状况等
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="coins"></param>
+        /// <param name="sheets"></param>
+        /// <returns></returns>
+        public int CountChange_DynamicPlan(int amount, int[] coins, int[] sheets = null)
         {
-            if (ways == null) ways = new int[money + 1];//这里都是整数，所以这样计算。其实是每一块钱都需要存下
-
-            // 3   1-2
+            if (sheets == null) sheets = new int[amount + 1];//这里都是整数，所以这样计算。其实是每一块钱都需要存下
 
             //遍历金额，找出每种金额对应的找零方式
-            for (int i = 1 ; i < ways.Length + 1 ; i++)//第一层：金额
+            for (int money = 1 ; money < sheets.Length ; money++)//第一层：金钱
             {
-                int minMoney = i;//临时变量
-                foreach (var item in coins)//第二层：找零方式
+                int minWays = money;//临时变量-已经金钱为money时，最少的方式数
+                foreach (var item in coins)//第二层：金钱面额
                 {
-                    if (i >= item)
+                    this.DynamicPlanNum++;
+                    if (money >= item)//当前的金钱必须要大于等于面额。 比如找补34块，面额不可能是50
                     {
-                        int temp_Money = ways[i - item] + 1;
-                        if (temp_Money < minMoney) minMoney = temp_Money;
+                        /*
+                         * 这里的+1是因为已经找补了一张面额为item的钱
+                         *
+                         * ways[25]=ways[20]种+[5元 1种]
+                         *
+                         */
+                        int temp_Money = sheets[money - item] + 1;
+
+                        //如果找到的值比当前最少的方式还少，则修正
+                        if (temp_Money < minWays)
+                            minWays = temp_Money;
                     }
                 }
 
-                ways[i] = minMoney;//把每种金额的找零方式，存起来
+                sheets[money] = minWays;//把每种金额的最少找零方式，存起来
             }
 
-            return ways[money];//
+            return sheets[amount];
         }
-
-        //ways[i - item]: 先把他自己前面的全部找到。比如5块钱去找， ways[5 - 4] 就找出4块钱对应的方式。
 
         #endregion 结算货币找零有多少种方式
     }
