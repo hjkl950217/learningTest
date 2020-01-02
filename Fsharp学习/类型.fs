ffccc038143fae70c40da87bcd48522061a62c53
CK <584880422@qq.com>
2020-01-02 09:54:54 +0800

    .

---
+++ Fsharp学习/类型.fs
@@ -0,0 +1,28 @@
+﻿module Fsharp学习.类型
+
+open Fsharp学习.类型模块
+
+let showType=
+
+    printfn "---演示-类型---"
+
+    //  实例化类型
+    let contact1 =
+        {
+            Name = "Alf"
+            Phone = "(206) 555-0157"
+            Verified = false
+        }
+
+    //也可以写成一行
+    let contact2= { Name="张三" ; Phone="123456789"; Verified=true  }
+
+    //复制contact1的值，只有phone是自己写的，其它是复制
+    let contactCopy=
+        { contact1 with
+            Phone="1234454458"
+        }
+
+    //可区分联合
+
+    printfn "ass"
---
+++ Fsharp学习/类型模块.fs
@@ -0,0 +1,33 @@
+﻿module Fsharp学习.类型模块
+
+///纪录类型-联系卡
+type ContactCard =
+    {
+        Name     : string
+        Phone    : string
+        Verified : bool
+    }
+
+//扑克牌的花色
+type Suit =
+     ///红桃
+     | Hearts
+     ///梅花
+     | Clubs
+     ///方块
+     | Diamonds
+     ///黑桃
+     | Spades
+
+//扑克牌的等级
+type Rank =
+       /// 代表 从 2 到 10
+       | Value of int
+       ///A
+       | Ace
+       ///K
+       | King
+       ///Q
+       | Queen
+       ///J
+       | Jack
