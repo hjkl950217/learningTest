7e9f54ddd8eb7058c7ba4833fcedbbf08350fdec
CK <584880422@qq.com>
2020-03-21 17:59:59 +0800

    添加测试代码

--- Verification.Core/Extensions/FPExtension.cs
+++ Verification.Core/Extensions/FPExtension.cs
@@ -4,15 +4,6 @@ namespace System
 {
     public static class FPExtension
     {
-        private static void TempTest()
-        {
-            Func<string, int> a = t => 1;
-            Func<int, bool> b = t => true;
-            Func<bool, double> c = t => 1.2;
-
-            Func<string, double> result = a.Pipe(b).Pipe(c);
-        }
-
         #region 管道
 
         /// <summary>
@@ -40,14 +31,13 @@ namespace System
         /// 适合:  a->b->void  得到 a->void 函数
         /// </summary>
         /// <typeparam name="TInput"></typeparam>
-        /// <typeparam name="TCenter"></typeparam>
         /// <typeparam name="TResult"></typeparam>
         /// <param name="sourceFunc"></param>
         /// <param name="action"></param>
         /// <returns></returns>
-        public static Action<TInput> Pipe<TInput, TCenter, TResult>(
-            this Func<TInput, TCenter> sourceFunc,
-            [NotNull] Action<TCenter> action)
+        public static Action<TInput> Pipe<TInput, TResult>(
+            this Func<TInput, TResult> sourceFunc,
+            [NotNull] Action<TResult> action)
         {
             sourceFunc.CheckNull(nameof(sourceFunc));
             action.CheckNull(nameof(action));
@@ -57,7 +47,7 @@ namespace System
 
         /// <summary>
         /// 管道 <para></para>
-        /// 适合:  a->b->...->void  得到 a->void 函数
+        /// 适合:  a->...->void  得到 a->void 函数
         /// </summary>
         /// <typeparam name="TInput"></typeparam>
         /// <param name="sourceFunc"></param>
@@ -71,6 +61,29 @@ namespace System
             actions.CheckNull(nameof(actions));
 
             return t => actions.For(item => item(t));
+        }
+
+        /// <summary>
+        /// 管道 <para></para>
+        /// 适合:  (a->c)->...->void  得到 a->void 函数
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sourceFunc"></param>
+        /// <param name="actions"></param>
+        /// <returns></returns>
+        public static Action<TInput> Pipe<TInput, TResult>(
+            this Func<TInput, TResult> sourceFunc,
+            [NotNull] params Action<TResult>[] actions)
+        {
+            sourceFunc.CheckNull(nameof(sourceFunc));
+            actions.CheckNull(nameof(actions));
+
+            return t =>
+            {
+                TResult tempResult = sourceFunc(t);
+                actions.For(item => item(tempResult));
+            };
         }
 
         #endregion 管道
