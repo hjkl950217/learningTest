6d2bba64c8b50697cda5696b73a523ca0058754b
CK <584880422@qq.com>
2020-03-21 15:28:26 +0800

    添加管道

--- Verification.Core/Extensions/FPExtension.cs
+++ Verification.Core/Extensions/FPExtension.cs
@@ -1,64 +1,78 @@
-﻿namespace System
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System
 {
     public static class FPExtension
     {
-        //public static Func<TInput, TResult> CreatePipe<TInput, TResult>(
-        //    this Func<TInput, TResult> sourceFunc,
-        //    Func<TInput, TResult> func)
-        //{
-        //}
+        private static void TempTest()
+        {
+            Func<string, int> a = t => 1;
+            Func<int, bool> b = t => true;
+            Func<bool, double> c = t => 1.2;
 
-        ///// <summary>
-        ///// 创建管道
-        ///// </summary>
-        ///// <typeparam name="TInput">输入参数类型</typeparam>
-        ///// <typeparam name="TResult">输出结果类型</typeparam>
-        ///// <param name="predicate">如果判断输出结果是否正确.不判断时请传递null</param>
-        ///// <param name="errorAction">判断失败时，如何处理？如果传递null或<paramref name="predicate"/>也为null，则不会执行</param>
-        ///// <param name="aggregate">累计函数，指示如何累加</param>
-        ///// <returns></returns>
-        //public static Func<TInput, TResult> CreatePipe<TInput, TResult>(
-        //    Func<TResult, bool> predicate,
-        //    Action<TInput, TResult> errorAction,
-        //    Func<TResult, TResult> aggregate,
-        //    params Func<TInput, TResult>[] funcs)
-        //{
-        //    errorAction = errorAction == null || predicate == null ? (i, r) => { } : errorAction;
-        //    predicate ??= t => true;
-        //    funcs ??= Array.Empty<Func<TInput, TResult>>();
+            Func<string, double> result = a.Pipe(b).Pipe(c);
+        }
 
-        //    return input =>
-        //    {
-        //        TResult result = default;
-        //        foreach (var item in funcs)
-        //        {
-        //            result = item(input);
+        #region 管道
 
-        //            if (!predicate(result)) errorAction(input, result);
-        //        }
-        //        return result;
-        //    };
-        //}
+        /// <summary>
+        /// 管道 <para></para>
+        /// 适合:  a->b->c  得到 a->c 函数
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TCenter"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sourceFunc"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static Func<TInput, TResult> Pipe<TInput, TCenter, TResult>(
+          this Func<TInput, TCenter> sourceFunc,
+          [NotNull] Func<TCenter, TResult> func)
+        {
+            sourceFunc.CheckNull(nameof(sourceFunc));
+            func.CheckNull(nameof(func));
 
-        ///// <summary>
-        ///// 用于快速创建累加器的方法。使用时传递需要的部分即可
-        ///// </summary>
-        ///// <typeparam name="TResult"></typeparam>
-        ///// <param name="seed"></param>
-        ///// <param name="func"></param>
-        ///// <param name="resultSelter"></param>
-        ///// <returns></returns>
-        //private static Func<TResult, TResult> FastCreateAggregater<TResult>(
-        //    [NotNull] Func<TResult, TResult, TResult> func,
-        //    TResult seed = default,
-        //    Func<TResult, TResult>? resultSelter = null
-        //    )
-        //{
-        //    //  System.Linq.Enumerable.Aggregate
+            return t => func(sourceFunc(t)); ;
+        }
 
-        //    resultSelter ??= t => t;
-        //    TResult result = seed;
-        //    return (next) => resultSelter(func(result, next));
-        //}
+        /// <summary>
+        /// 管道 <para></para>
+        /// 适合:  a->b->void  得到 a->void 函数
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TCenter"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sourceFunc"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Action<TInput> Pipe<TInput, TCenter, TResult>(
+            this Func<TInput, TCenter> sourceFunc,
+            [NotNull] Action<TCenter> action)
+        {
+            sourceFunc.CheckNull(nameof(sourceFunc));
+            action.CheckNull(nameof(action));
+
+            return t => action(sourceFunc(t));
+        }
+
+        /// <summary>
+        /// 管道 <para></para>
+        /// 适合:  a->b->...->void  得到 a->void 函数
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <param name="sourceFunc"></param>
+        /// <param name="actions"></param>
+        /// <returns></returns>
+        public static Action<TInput> Pipe<TInput>(
+            this Action<TInput> sourceFunc,
+            [NotNull] params Action<TInput>[] actions)
+        {
+            sourceFunc.CheckNull(nameof(sourceFunc));
+            actions.CheckNull(nameof(actions));
+
+            return t => actions.For(item => item(t));
+        }
+
+        #endregion 管道
     }
 }
