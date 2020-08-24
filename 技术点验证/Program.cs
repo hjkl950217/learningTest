2305e966a64e2e24756196d3fde0a16ccd462290
长空X <584880422@qq.com>
2020-08-24 21:42:49 +0800

    添加快速判断Int是否存在数组

--- 技术点验证/Program.cs
+++ 技术点验证/Program.cs
@@ -8,7 +8,7 @@ namespace 技术点验证
         public static void Main(string[]? args)
         {
             //开始验证
-            VerificationHelper.StartVerification(VerificationTypeEnum.A31_快速转换枚举为字典, args);
+            VerificationHelper.StartVerification(VerificationTypeEnum.A33_快速判断Int是否存在数组, args);
         }
     }
 }
