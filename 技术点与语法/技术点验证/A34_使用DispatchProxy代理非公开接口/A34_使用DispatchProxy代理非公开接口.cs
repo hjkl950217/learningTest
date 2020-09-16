efb6805a688c1fb10860934d2326bf1fedb85386
hjkl950217 <584880422@qq.com>
2020-09-16 16:47:37 +0800

    添加A34_使用DispatchProxy代理非公开接口，未实现

---
+++ 技术点与语法/技术点验证/A34_使用DispatchProxy代理非公开接口/A34_使用DispatchProxy代理非公开接口.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Verification.Core;
+
+namespace 技术点验证
+{
+    [VerifcationType(VerificationTypeEnum.A34_使用DispatchProxy代理非公开接口)]
+    public class A34_使用DispatchProxy代理非公开接口 : IVerification
+    {
+        public void Start(string[]? args)
+        {
+            //参考资料：https://www.cnblogs.com/lwqlun/p/11575686.html
+
+            throw new NotImplementedException();
+        }
+    }
+}
