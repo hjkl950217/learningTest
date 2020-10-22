c900cfa74585d3889be7f9240efa271e28c581fc
长空X <584880422@qq.com>
2020-10-22 13:37:23 +0800

    分离datetime dateitmeOffset

---
+++ CkTools/Src/CkTools.BaseExtensions/BaseType/DateTimeOffsetExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace System
+{
+    /// <summary>
+    /// <see cref="DateTimeOffset"/>的扩展类
+    /// </summary>
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// 转换为Unix时间戳,单位为秒（前端默认是s）
+        /// </summary>
+        /// <param name="source"></param>
+        /// <remarks>建议传递格林威治时间,从1970-01-01T00:00:00.000Z到现在的时间戳</remarks>
+        /// <returns></returns>
+        public static long ToUnixTimestamp(this DateTimeOffset source)
+        {
+            return source.ToUnixTimeSeconds();
+        }
+
+        /// <summary>
+        /// 在当前时间上面添加偏移
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public static DateTimeOffset AddOffset(this DateTimeOffset source, TimeSpan offset)
+        {
+            return new DateTimeOffset(source.UtcDateTime.Ticks, source.Offset + offset);
+        }
+    }
+}
--- CkTools/Src/CkTools.BaseExtensions/Helper/TypeConvertDelegate.cs
+++ CkTools/Src/CkTools.BaseExtensions/Helper/TypeConvertDelegate.cs
@@ -5,18 +5,34 @@
         #region 基础转换
 
         public static Func<string, long> stringToLong = System.Convert.ToInt64;
+
+        /// <summary>
+        /// int转bool,大于0为true
+        /// </summary>
+        public static Func<int, bool> intToBool = t => t > 0 ? true : false;
+
+        #region To DateTimeOffset
+
         public static Func<string, DateTimeOffset> stringToDateTimeOffset = DateTimeOffset.Parse;
         public static Func<long, DateTimeOffset> longToUtcDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds;
         public static Func<long, DateTimeOffset> longToUtcDateTimeOffsetByMilliseconds = DateTimeOffset.FromUnixTimeMilliseconds;
         public static Func<DateTimeOffset, DateTimeOffset> utcToLocal = t => t.AddOffset(TimeZoneInfo.Local.BaseUtcOffset);
         public static Func<DateTimeOffset, DateTimeOffset> localToUtc = t => t.AddOffset(-TimeZoneInfo.Local.BaseUtcOffset);
 
-        #endregion 基础转换
+        #endregion To DateTimeOffset
+
+        #region To DateTime
 
         /// <summary>
-        /// int转bool,大于0为true
+        ///
         /// </summary>
-        public static Func<int, bool> intToBool = t => t > 0 ? true : false;
+        public static Func<long, DateTime> longToDatetime =>
+          ts => TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1), TimeZoneInfo.Local)
+               + TimeSpan.FromSeconds(ts);
+
+        #endregion To DateTime
+
+        #endregion 基础转换
 
         #region 组合转换
 
