4ae99d9fe62183e99de420e1f2b378e4649248ed
王学懿 <xueyi.wang@macrowing.com>
2021-04-29 18:23:12 +0800

    添加freeid实验代码

---
+++ 技术点与语法/技术点验证/A40_任意长度2进制操作/A40_任意长度2进制操作.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Verification.Core;
+
+namespace 技术点验证.A40_任意长度2进制操作
+{
+    [VerifcationType(VerificationTypeEnum.A40_任意长度2进制操作)]
+    public class A40_任意长度2进制操作 : IVerification
+    {
+        public void Start(string[]? args)
+        {
+            Dictionary<int, ulong>? aaa = BitHelper.BitMap;
+
+            bool[]? test = BitHelper.ToBoolArray(3L);
+            ulong test2 = BitHelper.ToUlong(test);
+
+            //long result = this.Test2(2, 4, l1, 3);
+
+            //string resultStr = Convert.ToString(result, 2);
+            //Console.WriteLine(resultStr);
+        }
+
+        public void Test()
+        {
+        }
+    }
+
+    public static class BitHelper
+    {
+        public const int SnowflakeIDLength = 64;
+
+        private static Dictionary<int, ulong> bitMap;
+
+        /// <summary>
+        /// 记录2进制中对应长度全为1的值
+        /// <para>1=1</para>
+        /// <para>2=3</para>
+        /// <para>3=7</para>
+        /// </summary>
+        public static Dictionary<int, ulong> BitMap
+        {
+            get
+            {
+                if (bitMap == null)
+                {
+                    bitMap = new Dictionary<int, ulong>(SnowflakeIDLength);
+                    for (int i = 1 ; i < SnowflakeIDLength + 1 ; i++)
+                    {
+                        bitMap[i] = ulong.MaxValue >> (SnowflakeIDLength - i);
+                    }
+                    return bitMap;
+                }
+                else
+                {
+                    return bitMap;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将<paramref name="source"/>中的每一个2进制位转换为<see cref="bool"/>
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool[] ToBoolArray(ulong source)
+        {
+            bool[] result = new bool[SnowflakeIDLength];
+
+            for (int i = 0 ; i < result.Length ; i++)
+            {
+                result[i] = (source | BitHelper.BitMap[i + 1]) == source;
+            }
+            return result;
+        }
+
+        public static ulong ToUlong(bool[] source)
+        {
+            if (source.Length != SnowflakeIDLength)
+            {
+                throw new ArgumentException("必须为64个长度");
+            }
+
+            ulong result = ulong.MinValue;
+            for (int i = 0 ; i < SnowflakeIDLength ; i++)
+            {
+                result = source[i]
+                    ? (result | BitHelper.BitMap[i + 1])
+                    : result;
+            }
+
+            return result;
+        }
+    }
+}
--- 技术点与语法/技术点验证/Program.cs
+++ 技术点与语法/技术点验证/Program.cs
@@ -3,12 +3,12 @@ using Verification.Core.VerificationCore;
 
 namespace 技术点验证
 {
-	public class Program
-	{
-		public static void Main(string[]? args)
-		{
-			//开始验证
-			VerificationHelper.StartVerification(VerificationTypeEnum.A38_Task不等待返回, args);
-		}
-	}
+    public class Program
+    {
+        public static void Main(string[]? args)
+        {
+            //开始验证
+            VerificationHelper.StartVerification(VerificationTypeEnum.A40_任意长度2进制操作, args);
+        }
+    }
 }
