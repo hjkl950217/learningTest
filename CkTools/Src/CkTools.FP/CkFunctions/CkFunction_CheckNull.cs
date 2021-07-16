81c7fd1f011dd779af4e5f14752ef7b35f7ea7a3
王学懿 <xueyi.wang@macrowing.com>
2021-07-16 17:18:55 +0800

    增加判空的FP方法

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_CheckNull.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_CheckNull.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace CkTools.FP
 {
@@ -7,22 +9,9 @@ namespace CkTools.FP
     /// </summary>
     public static partial class CkFunctions
     {
-        public static void CheckNull<T1>(T1 exp1)
+        #region CheckNullWithException
 
-            where T1 : class
-        {
-            exp1.CheckNullWithException(nameof(exp1));
-        }
-
-        public static void CheckNull<T2, T1>(T2 exp2, T1 exp1)
-            where T2 : class
-            where T1 : class
-        {
-            exp2.CheckNullWithException(nameof(exp2));
-            exp1.CheckNullWithException(nameof(exp1));
-        }
-
-        public static void CheckNull<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+        public static void CheckNullWithException<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
             where T3 : class
             where T2 : class
             where T1 : class
@@ -32,7 +21,21 @@ namespace CkTools.FP
             exp1.CheckNullWithException(nameof(exp1));
         }
 
-        public static void CheckNull(params object[] exps)
+        public static void CheckNullWithException<T2, T1>(T2 exp2, T1 exp1)
+            where T2 : class
+            where T1 : class
+        {
+            exp2.CheckNullWithException(nameof(exp2));
+            exp1.CheckNullWithException(nameof(exp1));
+        }
+
+        public static void CheckNullWithException<T1>(T1 exp1)
+            where T1 : class
+        {
+            exp1.CheckNullWithException(nameof(exp1));
+        }
+
+        public static void CheckNullWithException(params object[] exps)
         {
             exps.CheckNullWithException(nameof(exps));
             foreach (object? exp in exps)
@@ -40,5 +43,153 @@ namespace CkTools.FP
                 exp.CheckNullWithException(nameof(exp));
             }
         }
+
+        #endregion CheckNullWithException
+
+        #region CheckNullOrEmpty
+
+        public static void CheckNullOrEmptyWithException<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+            where T3 : IEnumerable
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            exp1.CheckNullOrEmptyWithException(nameof(exp1));
+        }
+
+        public static void CheckNullOrEmptyWithException<T2, T1>(T2 exp2, T1 exp1)
+
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            exp1.CheckNullOrEmptyWithException(nameof(exp1));
+        }
+
+        public static void CheckNullOrEmptyWithException<T1>(T1 exp1)
+            where T1 : IEnumerable
+        {
+            exp1.CheckNullOrEmptyWithException(nameof(exp1));
+        }
+
+        public static void CheckNullOrEmptyWithException<T>(params T[] exps)
+            where T : IEnumerable
+        {
+            exps.CheckNullOrEmptyWithException(nameof(exps));
+            foreach (IEnumerable exp in exps)
+            {
+                exp.CheckNullOrEmptyWithException(nameof(exp));
+            }
+        }
+
+        #endregion CheckNullOrEmpty
+
+        #region IsNull
+
+        public static bool IsNull<T>(params T[] exps)
+        {
+            return exps == null || exps.Any(t => t == null);
+        }
+
+        public static bool IsNull<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+        {
+            return exp1 == null || exp2 == null || exp3 == null;
+        }
+
+        public static bool IsNull<T2, T1>(T2 exp2, T1 exp1)
+        {
+            return exp1 == null || exp2 == null;
+        }
+
+        public static bool IsNull<T1>(T1 exp1)
+        {
+            return exp1 == null;
+        }
+
+        #endregion IsNull
+
+        #region IsNotNull
+
+        public static bool IsNotNull<T>(params T[] exps)
+        {
+            return exps != null && exps.All(t => t != null);
+        }
+
+        public static bool IsNotNull<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+        {
+            return exp1 != null && exp2 != null && exp3 != null;
+        }
+
+        public static bool IsNotNull<T2, T1>(T2 exp2, T1 exp1)
+        {
+            return exp1 != null && exp2 != null;
+        }
+
+        public static bool IsNotNull<T1>(T1 exp1)
+        {
+            return exp1 != null;
+        }
+
+        #endregion IsNotNull
+
+        #region IsNullOrEmpty
+
+        public static bool IsNullOrEmpty<T>(params T[] exps)
+            where T : IEnumerable
+        {
+            return exps.IsNullOrEmpty() || exps.Any(t => t.IsNullOrEmpty());
+        }
+
+        public static bool IsNullOrEmpty<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+            where T3 : IEnumerable
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            return exp1.IsNullOrEmpty() || exp2.IsNullOrEmpty() || exp3.IsNullOrEmpty();
+        }
+
+        public static bool IsNullOrEmpty<T2, T1>(T2 exp2, T1 exp1)
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            return exp1.IsNullOrEmpty() || exp2.IsNullOrEmpty();
+        }
+
+        public static bool IsNullOrEmpty<T1>(T1 exp1)
+            where T1 : IEnumerable
+        {
+            return exp1.IsNullOrEmpty();
+        }
+
+        #endregion IsNullOrEmpty
+
+        #region IsNotNullOrEmpty
+
+        public static bool IsNotNullOrEmpty<T>(params T[] exps)
+            where T : IEnumerable
+        {
+            return exps.IsNotNullOrEmpty() && exps.All(t => t.IsNotNullOrEmpty());
+        }
+
+        public static bool IsNotNullOrEmpty<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
+            where T3 : IEnumerable
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            return exp1.IsNotNullOrEmpty() && exp2.IsNotNullOrEmpty() && exp3.IsNotNullOrEmpty();
+        }
+
+        public static bool IsNotNullOrEmpty<T2, T1>(T2 exp2, T1 exp1)
+            where T2 : IEnumerable
+            where T1 : IEnumerable
+        {
+            return exp1.IsNotNullOrEmpty() && exp2.IsNotNullOrEmpty();
+        }
+
+        public static bool IsNotNullOrEmpty<T1>(T1 exp1)
+            where T1 : IEnumerable
+        {
+            return exp1.IsNotNullOrEmpty();
+        }
+
+        #endregion IsNotNullOrEmpty
     }
 }
