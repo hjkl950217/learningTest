357df86aedfbd539af1c85b816a7adfbcd5a4747
王学懿 <xueyi.wang@macrowing.com>
2021-07-22 14:04:51 +0800

    优化方法执行器

--- CkTools/Src/CkTools.FP/Executer/ActionExecuter.cs
+++ CkTools/Src/CkTools.FP/Executer/ActionExecuter.cs
@@ -6,8 +6,10 @@ namespace CkTools.FP.Executer
     /// <summary>
     /// 方法执行器
     /// </summary>
-    public sealed class ActionExecuter
+    public class ActionExecuter
     {
+        #region 属性区
+
         public List<Action> StepList { get; internal set; }
         public static Action NullAction = () => { };
 
@@ -16,14 +18,18 @@ namespace CkTools.FP.Executer
         /// </summary>
         public bool IsEnd { get; internal set; }
 
+        #endregion 属性区
+
         /// <summary>
         /// 初始化<see cref="ActionExecuter"/>
         /// </summary>
-        private ActionExecuter()
+        protected ActionExecuter()
         {
             this.StepList = new List<Action>();
             this.IsEnd = false;
         }
+
+        #region Init
 
         /// <summary>
         /// 初始化<see cref="ActionExecuter"/>
@@ -34,16 +40,52 @@ namespace CkTools.FP.Executer
         }
 
         /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+        /// <param name="defaultResultFunc">传递一个委托用于初始化返回值</param>
+        /// <returns></returns>
+        public static ActionExecuter<TResult> Init<TResult>(Func<TResult>? defaultResultFunc)
+        {
+            return ActionExecuter<TResult>.Init(defaultResultFunc);
+        }
+
+        /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+        /// <param name="defaultResult">传递一个用于初始化返回值</param>
+        /// <returns></returns>
+        public static ActionExecuter<TResult> Init<TResult>(TResult defaultResult)
+        {
+            return ActionExecuter<TResult>.Init(() => defaultResult);
+        }
+
+        /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+        /// <returns></returns>
+        public static ActionExecuter<TResult> Init<TResult>() where TResult : class, new()
+
+        {
+            return ActionExecuter<TResult>.Init(() => new TResult());
+        }
+
+        #endregion Init
+
+        /// <summary>
         /// 执行
         /// </summary>
-        public void Execute()
+        public virtual void Execute()
         {
+            if (this.IsEnd) return;//用于幂等判定，结束后不再执行
+
             //执行中间步骤
             foreach (Action action in this.StepList)
             {
                 action();
-                if (this.IsEnd == true) return;
+                if (this.IsEnd) return;//方法执行后结束则直接返回
             }
+
+            this.IsEnd = true;//用于幂等判定，结束后不再执行
         }
     }
 }
---
+++ CkTools/Src/CkTools.FP/Executer/ActionExecuter`T.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CkTools.FP.Executer
+{
+#pragma warning disable CS8601 // 引用类型赋值可能为 null。
+#pragma warning disable CS8603 // 可能返回 null 引用。
+
+    /// <summary>
+    /// 方法执行器
+    /// </summary>
+    public class ActionExecuter<TResult> : ActionExecuter
+    {
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+
+        public TResult Result { get; set; }
+
+        /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+
+        protected ActionExecuter() : base()
+
+        {
+            this.Result = default;
+        }
+
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+
+        #region Init
+
+        /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+        /// <param name="defaultResultFunc">传递一个委托用于初始化返回值</param>
+        /// <returns></returns>
+        public static ActionExecuter<TResult> Init(Func<TResult>? defaultResultFunc)
+        {
+            TResult defaultResult = defaultResultFunc == null ? default : defaultResultFunc();
+
+            return new ActionExecuter<TResult>()
+            {
+                Result = defaultResult
+            };
+        }
+
+        /// <summary>
+        /// 初始化<see cref="ActionExecuter{TResult}"/>
+        /// </summary>
+        /// <returns></returns>
+        public new static ActionExecuter<TResult> Init()
+        {
+            return ActionExecuter<TResult>.Init(() => default);
+        }
+
+        #endregion Init
+
+        /// <summary>
+        /// 执行并获取结果
+        /// </summary>
+        /// <returns></returns>
+        public TResult ExecuteAndResult()
+        {
+            if (this.IsEnd)
+            {
+                return this.Result;
+            }
+            else
+            {
+                base.Execute();
+                return this.Result;
+            }
+        }
+    }
+
+#pragma warning restore CS8603 // 可能返回 null 引用。
+#pragma warning restore CS8601 // 引用类型赋值可能为 null。
+}
