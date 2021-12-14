d989ea33a6a7fe87aa2ec0b67a779f64c3d71084
王学懿 <xueyi.wang@macrowing.com>
2021-12-14 14:42:19 +0800

    优化BulidTree

--- CkTools/Src/CkTools.Abstraction/Helper/TreeHelper.cs
+++ CkTools/Src/CkTools.Abstraction/Helper/TreeHelper.cs
@@ -15,28 +15,28 @@ namespace CkTools.Helper
         /// <param name="parentIdGetter">父级id获取器</param>
         /// <param name="rootNodeJudge">根节点判断器，如:t=>t.id==null</param>
         /// <param name="nodeProcessor">节点处理器，指示子节点如何与父节点挂钩，如:(p,sub)=>p.Children.Add(sub)</param>
+        /// <param name="equalityComparer">比较器,默认使用<typeparamref name="T"/>的默认比较器</param>
         /// <returns></returns>
         public static List<T> BulidTree<T, TId>(
             IEnumerable<T> treeNodes,
             Func<T, TId> idGetter,
             Func<T, TId> parentIdGetter,
             Func<T, bool> rootNodeJudge,
-            Action<T, T> nodeProcessor)
+            Action<T, T> nodeProcessor,
+            IEqualityComparer<T> equalityComparer=null)
         {
+            equalityComparer??=EqualityComparer<T>.Default;
+            List<T> trees = new List<T>();
+
             try
             {
-                List<T> trees = new List<T>();
-
                 foreach (T treeNode in treeNodes)
                 {
-                    if (rootNodeJudge(treeNode))
-                    {
-                        trees.Add(treeNode);
-                    }
+                    if (rootNodeJudge(treeNode)) trees.Add(treeNode);
 
                     foreach (T it in treeNodes)
                     {
-                        if (object.Equals(parentIdGetter(it), idGetter(treeNode)))
+                        if (equalityComparer.Equals(parentIdGetter(it), idGetter(treeNode)))
                         {
                             nodeProcessor(treeNode, it);
                         }
