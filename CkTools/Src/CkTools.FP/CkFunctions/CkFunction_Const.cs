c1ce9dde3f0f66393cc95578eb26bbfbd4b714d6
王学懿 <xueyi.wang@macrowing.com>
2022-01-25 17:53:00 +0800

    增加2个记录日志的默认委托

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Const.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Const.cs
@@ -7,7 +7,12 @@ namespace CkTools.FP
     /// </summary>
     public static partial class CkFunctions
     {
+        public static Action<string> DefaultLog = debugInfo => Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} {debugInfo}");
+        public static Action DefaultLogTime = () => Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+
         public static Action EmtpyAction = () => { };
+        public static Func<bool> True = () => true;
+        public static Func<bool> False = () => false;
         public static Func<bool, Func<bool>> Bool = t => () => t;
         public static Func<int, Func<int>> Int = t => () => t;
         public static Func<string, Func<string>> String = t => () => t;
