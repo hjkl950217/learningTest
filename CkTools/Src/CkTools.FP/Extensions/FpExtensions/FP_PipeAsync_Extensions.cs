68aa04c9163f6ecf47a2caf1cb86df386b5d5bbb
王学懿 <xueyi.wang@macrowing.com>
2022-01-25 17:08:53 +0800

    屏蔽

--- CkTools/Src/CkTools.FP/Extensions/FpExtensions/FP_PipeAsync_Extensions.cs
+++ CkTools/Src/CkTools.FP/Extensions/FpExtensions/FP_PipeAsync_Extensions.cs
@@ -1,9 +1,9 @@
-﻿namespace System
-{
-    /// <summary>
-    /// 函数式扩展-管道异步
-    /// </summary>
-    public static class FP_PipeAsync_Extensions
-    {
-    }
-}
+﻿//namespace System
+//{
+//    /// <summary>
+//    /// 函数式扩展-管道异步
+//    /// </summary>
+//    public static class FP_PipeAsync_Extensions
+//    {
+//    }
+//}
