ce4dd5455e7cdb9be4952e54683e50ab12f71e38
王学懿 <xueyi.wang@macrowing.com>
2022-01-25 14:17:04 +0800

    增加几个单值转委托的方法

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Const.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Const.cs
@@ -8,5 +8,9 @@ namespace CkTools.FP
     public static partial class CkFunctions
     {
         public static Action EmtpyAction = () => { };
+        public static Func<bool, Func<bool>> Bool = t => () => t;
+        public static Func<int, Func<int>> Int = t => () => t;
+        public static Func<string, Func<string>> String = t => () => t;
+        public static Func<long, Func<long>> Long = t => () => t;
     }
 }
