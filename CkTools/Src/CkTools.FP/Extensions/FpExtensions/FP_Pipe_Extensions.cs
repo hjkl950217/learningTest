73f05f1a4d44eb16269e8535fcdcfbc50fa59faa
王学懿 <xueyi.wang@macrowing.com>
2022-01-25 17:12:03 +0800

    添加备注文档

--- CkTools/Src/CkTools.FP/Extensions/FpExtensions/FP_Pipe_Extensions.cs
+++ CkTools/Src/CkTools.FP/Extensions/FpExtensions/FP_Pipe_Extensions.cs
@@ -1,114 +1,112 @@
-﻿//using System.Diagnostics.CodeAnalysis;
-//using CkTools.FP;
+﻿using System.Diagnostics.CodeAnalysis;
+using CkTools.FP;
 
-//namespace System
-//{
-//    /// <summary>
-//    /// 函数式扩展-管道
-//    /// </summary>
-//    public static class FP_Pipe_Extensions
-//    {
-//        #region 返回Action
+namespace System
+{
+    /// <summary>
+    /// 函数式扩展-管道
+    /// </summary>
+    public static class FP_Pipe_Extensions
+    {
+        // (a->void)->(b->void)->...  => (a->void) <para></para>
+        // 示例:  (string->void)->(int->void)->...  => (string->void)
 
-//        #region 0个入参
+        #region Action扩展-0个入参
 
-//        /// <summary>
-//        /// 管道 <para></para>
-//        /// (a->void)->(b->void)->...  => (a->void) <para></para>
-//        /// 示例:  (string->void)->(int->void)->...  => (string->void)
-//        /// </summary>
-//        /// <typeparam name="TInput"></typeparam>
-//        /// <param name="sourceExp"></param>
-//        /// <param name="exps"></param>
-//        /// <returns></returns>
-//        public static Action<TInput> Pipe<TInput>(
-//            [NotNull] this Func<TInput> sourceExp,
-//            [NotNull] params Action<TInput>[] exps)
-//        {
-//            return CkFunctions.Pipe(sourceExp, exps);
-//        }
+        #endregion Action扩展-0个入参
 
-//        #endregion 0个入参
+        #region Action扩展-1个入参
 
-//        #region 1个入参
+        public static Action<TInput> Pipe<TInput>(
+            [NotNull] this Action<TInput> sourceExp,
+            [NotNull] params Action<TInput>[] exps)
+        {
+            CkFunctions.CheckNullWithException(sourceExp);
+            CkFunctions.CheckNullWithException(exps);
 
-//        ///// <summary>
-//        ///// 管道 <para></para>
-//        ///// (a->b)->(b->void)->...  => (a->void) <para></para>
-//        ///// 示例:  (string->int)->(int->void)->...  => (string->void)
-//        ///// </summary>
-//        ///// <typeparam name="TInput"></typeparam>
-//        ///// <typeparam name="TResult"></typeparam>
-//        ///// <param name="sourceExp"></param>
-//        ///// <param name="exps"></param>
-//        ///// <returns></returns>
-//        //public static Action<TInput> Pipe<TInput, TResult>(
-//        //    [NotNull] this Func<TInput, TResult> sourceExp,
-//        //    [NotNull] params Action<TResult>[] exps)
-//        //{
-//        //    return CkFunctions.Pipe(sourceExp, exps);
-//        //}
+            return t =>
+            {
+                sourceExp(t);
+                for (int i = 0; i < exps.Length; i++)
+                {
+                    exps[i](t);
+                }
+            };
+        }
 
-//        #endregion 1个入参
+        #endregion Action扩展-1个入参
 
-//        #endregion 返回Action
+        #region Action扩展-2个入参
 
-//        #region 0个入参
+        #endregion Action扩展-2个入参
 
-//        /// <summary>
-//        /// 管道 <para></para>
-//        /// (a->void)->(b->void)->...  => (a->void) <para></para>
-//        /// 示例:  (string->void)->(int->void)->...  => (string->void)
-//        /// </summary>
-//        /// <typeparam name="TInput"></typeparam>
-//        /// <param name="sourceExp"></param>
-//        /// <param name="exps"></param>
-//        /// <returns></returns>
-//        public static Action<TInput> Pipe<TInput>(
-//            [NotNull] this Action<TInput> sourceExp,
-//            [NotNull] params Action<TInput>[] exps)
-//        {
-//            return CkFunctions.Pipe<TInput>(sourceExp, exps);
-//        }
+        #region Func-0个入参
 
-//        #endregion 0个入参
+        #endregion Func-0个入参
 
-//        #region 1个入参
+        #region Func-1个入参
 
-//        ///// <summary>
-//        ///// 管道
-//        ///// </summary>
-//        ///// <typeparam name="TInput"></typeparam>
-//        ///// <param name="sourceExp"></param>
-//        ///// <param name="exps"></param>
-//        ///// <returns></returns>
-//        //public static Action<TInput> Pipe<TInput>(
-//        //    [NotNull] this Action<TInput> sourceExp,
-//        //    [NotNull] params Action<TInput>[] exps)
-//        //{
-//        //    return CkFunctions.Pipe(sourceExp, exps);
-//        //}
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sourceExp"></param>
+        /// <param name="exps"></param>
+        /// <returns></returns>
+        public static Func<TResult> Pipe<TResult>(
+            [NotNull] this Func<TResult> sourceExp,
+            [NotNull] params Action<TResult>[] exps)
+        {
+            return CkFunctions.Pipe(exps, sourceExp);
+        }
 
-//        #endregion 1个入参
+        #endregion Func-1个入参
 
-//        #region 返回Func
+        ///// <summary>
+        ///// 管道 <para></para>
+        ///// (a->b)->(b->void)->...  => (a->void) <para></para>
+        ///// 示例:  (string->int)->(int->void)->...  => (string->void)
+        ///// </summary>
+        ///// <typeparam name="TInput"></typeparam>
+        ///// <typeparam name="TResult"></typeparam>
+        ///// <param name="sourceExp"></param>
+        ///// <param name="exps"></param>
+        ///// <returns></returns>
+        //public static Action<TInput> Pipe<TInput, TResult>(
+        //    [NotNull] this Func<TInput, TResult> sourceExp,
+        //    [NotNull] params Action<TResult>[] exps)
+        //{
+        //    return CkFunctions.Pipe(sourceExp, exps);
+        //}
 
-//        /// <summary>
-//        /// 管道
-//        /// </summary>
-//        /// <typeparam name="TInput"></typeparam>
-//        /// <typeparam name="TCenter"></typeparam>
-//        /// <typeparam name="TResult"></typeparam>
-//        /// <param name="sourceExp"></param>
-//        /// <param name="exp"></param>
-//        /// <returns></returns>
-//        public static Func<TInput, TResult> Pipe<TInput, TCenter, TResult>(
-//          [NotNull] this Func<TInput, TCenter> sourceExp,
-//          [NotNull] Func<TCenter, TResult> exp)
-//        {
-//            return CkFunctions.Pipe(sourceExp, exp);
-//        }
+        ///// <summary>
+        ///// 管道
+        ///// </summary>
+        ///// <typeparam name="TInput"></typeparam>
+        ///// <param name="sourceExp"></param>
+        ///// <param name="exps"></param>
+        ///// <returns></returns>
+        //public static Action<TInput> Pipe<TInput>(
+        //    [NotNull] this Action<TInput> sourceExp,
+        //    [NotNull] params Action<TInput>[] exps)
+        //{
+        //    return CkFunctions.Pipe(sourceExp, exps);
+        //}
 
-//        #endregion 返回Func
-//    }
-//}
+        /// <summary>
+        /// 管道
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TCenter"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sourceExp"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static Func<TInput, TResult> Pipe<TInput, TCenter, TResult>(
+          [NotNull] this Func<TInput, TCenter> sourceExp,
+          [NotNull] Func<TCenter, TResult> exp)
+        {
+            return CkFunctions.Pipe(sourceExp, exp);
+        }
+    }
+}
