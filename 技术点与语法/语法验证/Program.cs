e7670a0c93a9e9038bb3d97e45f55acd61cc368b
王学懿 <xueyi.wang@macrowing.com>
2022-01-27 13:23:37 +0800

    更新目标框架

--- 技术点与语法/语法验证/Program.cs
+++ 技术点与语法/语法验证/Program.cs
@@ -7,16 +7,6 @@ namespace 语法验证与学习
     {
         public static void Main(string[]? args)
         {
-            //Type type = typeof(B21_Task的Wait方法是否任务级暂停);
-            //Type[]? gifs = type.GetInterfaces();
-
-            //bool ttt = gifs.Any(t => t.GetGenericTypeDefinition() == typeof(IVerification<>));
-            //bool t1 = gifs[0].GetGenericTypeDefinition() == typeof(IVerification<>);
-            //bool t2 = gifs[1].IsGenericType && gifs[1].GetGenericTypeDefinition() == typeof(IVerification<>);
-
-            //Type[]? gArgs = gifs[0].GetGenericArguments();
-            //bool isTag = type == gArgs[0];
-
             //开始验证
             VerificationHelper.StartVerification(VerificationTypeEnum.B19_字符串驻留池, args);
         }
