d3212b7adcd0c18858ad6ff190344dcf402fde52
王学懿 <xueyi.wang@macrowing.com>
2022-05-10 15:49:15 +0800

    添加TryCreateFile

---
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_File.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace CkTools.FP
+{
+    /// <summary>
+    /// 函数式功能-文件
+    /// </summary>
+    public static partial class CkFunctions
+    {
+        public static Func<string, bool> IsFullFileName => fileName => Path.IsPathRooted(fileName) && Path.IsPathFullyQualified(fileName);
+
+        /// <summary>
+        /// 获取程序基目录
+        /// </summary>
+        public static Func<string> BaseAddress = () => AppDomain.CurrentDomain.BaseDirectory;
+
+        /// <summary>
+        /// 尝试创建文件<para></para>
+        /// 不包含地址: 使用程序基地址<para></para>
+        /// 不存在目标文件夹: 创建文件夹<para></para>
+        /// </summary>
+        /// <param name="fileName"></param>
+        public static void TryCreateFile(string fileName)
+        {
+            //存在直接返回
+            if (File.Exists(fileName))
+            {
+                return;
+            }
+
+            //补全地址
+            bool isFull = CkFunctions.IsFullFileName(fileName);
+            if (!isFull)
+            {
+                fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            }
+
+            //尝试创建文件夹
+            string dirPath = Path.GetDirectoryName(fileName);
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            //创建空白文件
+            File.Create(fileName);
+        }
+    }
+}
