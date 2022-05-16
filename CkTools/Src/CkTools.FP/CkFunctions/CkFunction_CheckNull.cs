77d5788c79fec2d14488d28b186b9eaa71d08446
王学懿 <xueyi.wang@macrowing.com>
2022-05-16 15:54:23 +0800

    重构判断检查空函数

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_CheckNull.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_CheckNull.cs
@@ -1,174 +1,172 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using CkTools.FP.Model;
 
 namespace CkTools.FP
 {
     /// <summary>
-    /// 函数式功能
+    /// 函数式功能-检查空
     /// </summary>
     public static partial class CkFunctions
     {
+        #region 公共
+
+        /// <summary>
+        /// 处理对象,返回一个处理对象的函数
+        /// </summary>
+        /// <value>
+        /// <para>函数1：判断函数,返回true时才会处理对象</para>
+        /// <para>函数2：处理函数</para>
+        /// </value>
+        public static Func<
+            Action<T>,
+            Action<T>> ProcessObject<T>(Func<T, bool> judge)
+        {
+            return
+                processObj =>
+                exp =>
+                {
+                    if (judge(exp))
+                    {
+                        processObj(exp);
+                    }
+                };
+        }
+
+        /// <summary>
+        /// 批量处理对象,返回一个处理对象集合的函数
+        /// </summary>
+        /// <value>
+        /// <para>函数1：判断函数,返回true时才会处理对象</para>
+        /// <para>函数2：处理函数,处理一个链表对象</para>
+        /// </value>
+        public static Func<
+            Action<LinkedList<Entry<T>>>,
+            Action<T[]>> BatchProcessObject<T>(Func<T, bool> judge)
+        {
+            return
+                processObjs =>
+                exps =>
+                {
+                    #region 异常检测
+
+                    if (judge == null)
+                    {
+                        throw new ArgumentNullException(nameof(judge));
+                    }
+
+                    if (processObjs == null)
+                    {
+                        throw new ArgumentNullException(nameof(processObjs));
+                    }
+
+                    if (exps == null)
+                    {
+                        throw new ArgumentNullException(nameof(exps));
+                    }
+
+                    #endregion 异常检测
+
+                    //判断
+                    LinkedList<Entry<T>> errorList = new LinkedList<Entry<T>>();
+                    for (int i = exps.Length - 1 ; i > -1 ; i--)
+                    {
+                        if (judge(exps[i]))
+                        {
+                            errorList.AddLast(Entry.Create(i, exps[i]));
+                        }
+                    }
+
+                    //处理
+                    if (errorList.Any())
+                    {
+                        processObjs(errorList);
+                    }
+                };
+        }
+
+        public static Func<LinkedList<Entry>, string> JoinIndex(string prefix = "exp")
+        {
+            return arr => string.Join(",", arr.Select(t => $"{prefix}{t.Index}"));
+        }
+
+        public static Func<LinkedList<Entry<T>>, string> JoinIndex2<T>(string prefix = "exp")
+        {
+            return arr => string.Join(",", arr.Select(t => $"{prefix}{t.Index}"));
+        }
+
+        /// <summary>
+        /// 默认,将条目聚集后抛出一个<see cref="ArgumentNullException"/>
+        /// </summary>
+        public static Action<LinkedList<Entry<T>>> DefaultBatchThrow<T>()
+        {
+            return errorArray
+                => throw new ArgumentNullException(CkFunctions.JoinIndex2<T>("exp")(errorArray));
+        }
+
+        /// <summary>
+        /// 默认,将条目聚集后抛出一个<see cref="ArgumentNullException"/>
+        /// </summary>
+        public static Action<LinkedList<Entry>> DefaultBatchThrow()
+        {
+            return errorArray
+                => throw new ArgumentNullException(string.Join(",", errorArray.Select(t => $"exp{t.Index}")));
+        }
+
+        #endregion 公共
+
         #region CheckNullWithException
 
-        public static void CheckNullWithException<T>(params T[] exps)
-            where T : class
-        {
-            if (exps == null) throw new ArgumentNullException(nameof(exps));
-            foreach (T? exp in exps)
-            {
-                if (exp == null) throw new ArgumentNullException(nameof(exp));
-            }
-        }
+        public static void CheckNullWithException(params object[] exps)
+        //{
+        //    if (exps == null)
+        //        throw new ArgumentNullException(nameof(exps));
 
-        public static void CheckNullWithException<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-            where T5 : class?
-            where T4 : class?
-            where T3 : class?
-            where T2 : class?
-            where T1 : class?
-        {
-            if (exp1 == null) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null) throw new ArgumentNullException(nameof(exp3));
-            if (exp4 == null) throw new ArgumentNullException(nameof(exp4));
-            if (exp5 == null) throw new ArgumentNullException(nameof(exp5));
-        }
+        //    for (int i = exps.Length - 1 ; i > -1 ; i--)
+        //    {
+        //        if (exps[i] == null)
+        //            throw new ArgumentNullException($"exp{i + 1}");
+        //    }
 
-        public static void CheckNullWithException<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
+        //    Action<object[]>? aa = CkFunctions.BatchCkeckNull(CkFunctions.IsNull)(DefaultThrowBatchNull);
+        //}
+         => CkFunctions.BatchProcessObject<object>(CkFunctions.IsNull)(CkFunctions.DefaultBatchThrow<object>())(exps);
 
-            where T4 : class?
-            where T3 : class?
-            where T2 : class?
-            where T1 : class?
-        {
-            if (exp1 == null) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null) throw new ArgumentNullException(nameof(exp3));
-            if (exp4 == null) throw new ArgumentNullException(nameof(exp4));
-        }
-
-        public static void CheckNullWithException<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-            where T3 : class?
-            where T2 : class?
-            where T1 : class?
-        {
-            if (exp1 == null) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null) throw new ArgumentNullException(nameof(exp3));
-        }
-
-        public static void CheckNullWithException<T2, T1>(T2 exp2, T1 exp1)
-            where T2 : class?
-            where T1 : class?
-        {
-            if (exp1 == null) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null) throw new ArgumentNullException(nameof(exp2));
-        }
-
-        public static void CheckNullWithException<T1>(T1 exp1)
-            where T1 : class?
-        {
-            if (exp1 == null) throw new ArgumentNullException(nameof(exp1));
-        }
+        /// <summary>
+        /// 检查函数,检查对象集合中是否有空值,有空值时抛出异常
+        /// </summary>
+        public static Action<object[]> CheckNullWithExceptionArray = CkFunctions.CheckNullWithException;
 
         #endregion CheckNullWithException
 
-        #region CheckNullOrEmpty
+        #region CheckNullOrEmptyWithException
 
-        public static void CheckNullOrEmptyWithException<T>(params T[] exps)
-            where T : IEnumerable
-        {
-            if (exps == null || !exps.Any()) throw new ArgumentNullException(nameof(exps));
-            foreach (IEnumerable exp in exps)
-            {
-                if (exp == null || !exp.Any()) throw new ArgumentNullException(nameof(exp));
-            }
-        }
+        public static void CheckNullOrEmptyWithException<T>(params IEnumerable[] exps)
+        //{
+        //    if (exps == null || !exps.Any())
+        //        throw new ArgumentNullException(nameof(exps));
+        //    foreach (IEnumerable exp in exps)
+        //    {
+        //        if (exp == null || !exp.Any())
+        //            throw new ArgumentNullException(nameof(exp));
+        //    }
+        //}
+        => CkFunctions.BatchProcessObject<IEnumerable>(CkFunctions.IsNullOrEmpty)(CkFunctions.DefaultBatchThrow<IEnumerable>())(exps);
 
-        public static void CheckNullOrEmptyWithException<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-            where T5 : IEnumerable
-            where T4 : IEnumerable
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null || !exp2.Any()) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null || !exp3.Any()) throw new ArgumentNullException(nameof(exp3));
-            if (exp4 == null || !exp4.Any()) throw new ArgumentNullException(nameof(exp4));
-            if (exp5 == null || !exp5.Any()) throw new ArgumentNullException(nameof(exp5));
-        }
-
-        public static void CheckNullOrEmptyWithException<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-
-            where T4 : IEnumerable
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null || !exp2.Any()) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null || !exp3.Any()) throw new ArgumentNullException(nameof(exp3));
-            if (exp4 == null || !exp4.Any()) throw new ArgumentNullException(nameof(exp4));
-        }
-
-        public static void CheckNullOrEmptyWithException<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null || !exp2.Any()) throw new ArgumentNullException(nameof(exp2));
-            if (exp3 == null || !exp3.Any()) throw new ArgumentNullException(nameof(exp3));
-        }
-
-        public static void CheckNullOrEmptyWithException<T2, T1>(T2 exp2, T1 exp1)
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) throw new ArgumentNullException(nameof(exp1));
-            if (exp2 == null || !exp2.Any()) throw new ArgumentNullException(nameof(exp2));
-        }
-
-        public static void CheckNullOrEmptyWithException<T1>(T1 exp1)
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) throw new ArgumentNullException(nameof(exp1));
-        }
-
-        #endregion CheckNullOrEmpty
+        #endregion CheckNullOrEmptyWithException
 
         #region IsNull
 
-        public static bool IsNull<T>(params T[] exps)
+        public static bool IsNull(params object[] exps)
         {
             return exps == null || exps.Any(t => t == null);
         }
 
-        public static bool IsNull<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
+        public static bool IsNull<T1>(T1 exp)
         {
-            return exp1 == null || exp2 == null || exp3 == null || exp4 == null || exp5 == null;
-        }
-
-        public static bool IsNull<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-        {
-            return exp1 == null || exp2 == null || exp3 == null || exp4 == null;
-        }
-
-        public static bool IsNull<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-        {
-            return exp1 == null || exp2 == null || exp3 == null;
-        }
-
-        public static bool IsNull<T2, T1>(T2 exp2, T1 exp1)
-        {
-            return exp1 == null || exp2 == null;
-        }
-
-        public static bool IsNull<T1>(T1 exp1)
-        {
-            return exp1 == null;
+            return exp == null;
         }
 
         #endregion IsNull
@@ -180,97 +178,26 @@ namespace CkTools.FP
             return !IsNull(exps);
         }
 
-        public static bool IsNotNull<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
+        public static bool IsNotNull<T1>(T1 exp)
         {
-            return !IsNull(exp5, exp4, exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNull<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-        {
-            return !IsNull(exp4, exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNull<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-        {
-            return !IsNull(exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNull<T2, T1>(T2 exp2, T1 exp1)
-        {
-            return !IsNull(exp1);
-        }
-
-        public static bool IsNotNull<T1>(T1 exp1)
-        {
-            return !IsNull(exp1);
+            return !IsNull(exp);
         }
 
         #endregion IsNotNull
 
         #region IsNullOrEmpty
 
-        public static bool IsNullOrEmpty<T>(params T[] exps)
-            where T : IEnumerable
+        public static bool IsNullOrEmpty(params IEnumerable[] exps)
         {
-            return exps == null || !exps.Any()
-                || exps.Any(t => t == null || !t.Any());
+            return exps == null //检查空引用
+                || !exps.Any()  //检查空集合
+                || exps.Any(CkFunctions.IsNullOrEmpty); //检查集合中的元素
         }
 
-        public static bool IsNullOrEmpty<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-            where T5 : IEnumerable
-            where T4 : IEnumerable
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
+        public static bool IsNullOrEmpty(IEnumerable exp)
         {
-            if (exp1 == null || !exp1.Any()) return true;
-            if (exp2 == null || !exp2.Any()) return true;
-            if (exp3 == null || !exp3.Any()) return true;
-            if (exp4 == null || !exp4.Any()) return true;
-            if (exp5 == null || !exp5.Any()) return true;
-            return false;
-        }
-
-        public static bool IsNullOrEmpty<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-
-            where T4 : IEnumerable
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) return true;
-            if (exp2 == null || !exp2.Any()) return true;
-            if (exp3 == null || !exp3.Any()) return true;
-            if (exp4 == null || !exp4.Any()) return true;
-
-            return false;
-        }
-
-        public static bool IsNullOrEmpty<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) return true;
-            if (exp2 == null || !exp2.Any()) return true;
-            if (exp3 == null || !exp3.Any()) return true;
-            return false;
-        }
-
-        public static bool IsNullOrEmpty<T2, T1>(T2 exp2, T1 exp1)
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) return true;
-            if (exp2 == null || !exp2.Any()) return true;
-            return false;
-        }
-
-        public static bool IsNullOrEmpty<T1>(T1 exp1)
-            where T1 : IEnumerable
-        {
-            if (exp1 == null || !exp1.Any()) return true;
-            return false;
+            return exp == null
+                || !exp.Any();
         }
 
         #endregion IsNullOrEmpty
@@ -281,42 +208,6 @@ namespace CkTools.FP
             where T : IEnumerable
         {
             return !IsNullOrEmpty(exps);
-        }
-
-        public static bool IsNotNullOrEmpty<T5, T4, T3, T2, T1>(T5 exp5, T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-            where T5 : IEnumerable
-            where T4 : IEnumerable
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-
-        {
-            return !IsNullOrEmpty(exp5, exp4, exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNullOrEmpty<T4, T3, T2, T1>(T4 exp4, T3 exp3, T2 exp2, T1 exp1)
-              where T4 : IEnumerable
-              where T3 : IEnumerable
-              where T2 : IEnumerable
-              where T1 : IEnumerable
-
-        {
-            return !IsNullOrEmpty(exp4, exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNullOrEmpty<T3, T2, T1>(T3 exp3, T2 exp2, T1 exp1)
-            where T3 : IEnumerable
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            return !IsNullOrEmpty(exp3, exp2, exp1);
-        }
-
-        public static bool IsNotNullOrEmpty<T2, T1>(T2 exp2, T1 exp1)
-            where T2 : IEnumerable
-            where T1 : IEnumerable
-        {
-            return !IsNullOrEmpty(exp2, exp1);
         }
 
         public static bool IsNotNullOrEmpty<T1>(T1 exp1)
