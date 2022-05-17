bcf361111366cda9e2fe308ca39fc52dd031dce3
王学懿 <xueyi.wang@macrowing.com>
2022-05-17 11:44:25 +0800

    调整分部函数

--- CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Partial.cs
+++ CkTools/Src/CkTools.FP/CkFunctions/CkFunction_Partial.cs
@@ -10,25 +10,25 @@ namespace CkTools.FP
         #region Action
 
         public static Action<T1> Partial<T1>(
-            Func<T1, Action> exp)
+            Action<T1> exp)
         {
-            return x => exp(x);
+            return exp;//这个函数是为了方便将对象的方法转换为委托
         }
 
         public static Action<T1, T2> Partial<T1, T2>(
-            Func<T1, Func<T2, Action>> exp)
+            Func<T1, Action<T2>> exp)
         {
             return (x, y) => exp(x)(y);
         }
 
         public static Action<T1, T2, T3> Partial<T1, T2, T3>(
-            Func<T1, Func<T2, Func<T3, Action>>> exp)
+            Func<T1, Func<T2, Action<T3>>> exp)
         {
             return (x, y, z) => exp(x)(y)(z);
         }
 
         public static Action<T1, T2, T3, T4> Partial<T1, T2, T3, T4>(
-            Func<T1, Func<T2, Func<T3, Func<T4, Action>>>> exp)
+            Func<T1, Func<T2, Func<T3, Action<T4>>>> exp)
         {
             return (x, y, z, g) => exp(x)(y)(z)(g);
         }
@@ -38,27 +38,27 @@ namespace CkTools.FP
         #region Func
 
         public static Func<T1, TResult> Partial<T1, TResult>(
-            Func<T1, Func<TResult>> exp)
+            Func<T1, TResult> exp)
         {
-            return x => exp(x)();
+            return exp;//这个函数是为了方便将对象的方法转换为委托
         }
 
         public static Func<T1, T2, TResult> Partial<T1, T2, TResult>(
-            Func<T1, Func<T2, Func<TResult>>> exp)
+            Func<T1, Func<T2, TResult>> exp)
         {
-            return (x, y) => exp(x)(y)();
+            return (x, y) => exp(x)(y);
         }
 
         public static Func<T1, T2, T3, TResult> Partial<T1, T2, T3, TResult>(
-            Func<T1, Func<T2, Func<T3, Func<TResult>>>> exp)
+            Func<T1, Func<T2, Func<T3, TResult>>> exp)
         {
-            return (x, y, z) => exp(x)(y)(z)();
+            return (x, y, z) => exp(x)(y)(z);
         }
 
         public static Func<T1, T2, T3, T4, TResult> Partial<T1, T2, T3, T4, TResult>(
-            Func<T1, Func<T2, Func<T3, Func<T4, Func<TResult>>>>> exp)
+            Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> exp)
         {
-            return (x, y, z, g) => exp(x)(y)(z)(g)();
+            return (x, y, z, g) => exp(x)(y)(z)(g);
         }
 
         #endregion Func
--- CkTools/Src/CkTools.FP/Extensions/FpExtensions/Fp_Partial_Extensions.cs
+++ CkTools/Src/CkTools.FP/Extensions/FpExtensions/Fp_Partial_Extensions.cs
@@ -10,25 +10,25 @@ namespace System
         #region Action
 
         public static Action<T1> Partial<T1>(
-            this Func<T1, Action> exp)
+            this Action<T1> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Action<T1, T2> Partial<T1, T2>(
-            this Func<T1, Func<T2, Action>> exp)
+            this Func<T1, Action<T2>> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Action<T1, T2, T3> Partial<T1, T2, T3>(
-            this Func<T1, Func<T2, Func<T3, Action>>> exp)
+            this Func<T1, Func<T2, Action<T3>>> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Action<T1, T2, T3, T4> Partial<T1, T2, T3, T4>(
-            this Func<T1, Func<T2, Func<T3, Func<T4, Action>>>> exp)
+            this Func<T1, Func<T2, Func<T3, Action<T4>>>> exp)
         {
             return CkFunctions.Partial(exp);
         }
@@ -38,25 +38,25 @@ namespace System
         #region Func
 
         public static Func<T1, TResult> Partial<T1, TResult>(
-            this Func<T1, Func<TResult>> exp)
+            this Func<T1, TResult> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Func<T1, T2, TResult> Partial<T1, T2, TResult>(
-            this Func<T1, Func<T2, Func<TResult>>> exp)
+            this Func<T1, Func<T2, TResult>> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Func<T1, T2, T3, TResult> Partial<T1, T2, T3, TResult>(
-            this Func<T1, Func<T2, Func<T3, Func<TResult>>>> exp)
+            this Func<T1, Func<T2, Func<T3, TResult>>> exp)
         {
             return CkFunctions.Partial(exp);
         }
 
         public static Func<T1, T2, T3, T4, TResult> Partial<T1, T2, T3, T4, TResult>(
-            this Func<T1, Func<T2, Func<T3, Func<T4, Func<TResult>>>>> exp)
+            this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> exp)
         {
             return CkFunctions.Partial(exp);
         }
