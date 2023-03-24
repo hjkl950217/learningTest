1276568900931c9d213532e7ffc76376dc46e5d0
王学懿 <xueyi.wang@macrowing.com>
2023-03-24 15:40:15 +0800

    添加删除obj方法

---
+++ 技术点与语法/工具/删除bin和obj/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        string folderPath = @"E:\公司\成都-鸿翼医药\V5"; // 替换为您要扫描的文件夹路径
+
+        string[] includePatterns = { "obj", "bin" };
+
+        string[] directories = Directory
+            .GetDirectories(folderPath, "*", SearchOption.AllDirectories)
+            .Where(t => includePatterns.Any(p => IsMatch(t, p)))
+            .ToArray();
+
+        foreach(string directory in directories)
+        {
+            Directory.Delete(directory, true); // 删除文件夹及其所有内容
+            Console.WriteLine("删除文件夹: " + directory); // 输出已删除的文件夹名
+        }
+
+        Console.WriteLine($"完成，删除了{directories.Length}个文件夹");
+        Console.ReadLine();
+    }
+
+    public static bool IsMatch(string str, string matchStr)
+    {
+        return str.EndsWith(matchStr);
+    }
+}
