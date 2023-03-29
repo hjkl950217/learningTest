e1b95c28241ee8e2f17ef5bbdc121cdea8bc5ab2
王学懿 <xueyi.wang@macrowing.com>
2023-03-29 16:54:21 +0800

    更新Vue学习

--- Vue学习/vue-basic/js/tool.js
+++ Vue学习/vue-basic/js/tool.js
@@ -1,35 +1,20 @@
 //排序-升序
 Array.prototype.orderBy = function (propFunc) {
-    return this.sort((a, b) => {
-        const propA = propFunc(a);
-        const propB = propFunc(b);
-
-        if (propA < propB) {
-            return -1;
-        } else if (propA > propB) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-
-    });
+    if (!propFunc) {
+        return this.slice();
+    }
+    else {
+        return this.slice()
+            .sort((a, b) => propFunc(a) - propFunc(b));
+    }
 };
 
 //排序-降序
 Array.prototype.orderByDesc = function (propFunc) {
-    return this.sort((a, b) => {
-        const propA = propFunc(a);
-        const propB = propFunc(b);
-
-        if (propA < propB) {
-            return 1;
-        } else if (propA > propB) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-
-    });
+    if (!propFunc) {
+        return this.slice();
+    }
+    else {
+        return this.slice().sort((a, b) => propFunc(b) - propFunc(a));
+    }
 };
