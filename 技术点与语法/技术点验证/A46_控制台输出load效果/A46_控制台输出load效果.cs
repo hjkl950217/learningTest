6a0679c9cfc8c76bd5ddbf32f568c591e8b7e77e
王学懿 <xueyi.wang@macrowing.com>
2023-05-30 18:34:44 +0800

    添加控制台load效果

---
+++ 技术点与语法/技术点验证/A46_控制台输出load效果/A46_控制台输出load效果.cs
@@ -0,0 +1,48 @@
+﻿namespace 技术点验证
+{
+    [VerifcationType(VerificationTypeEnum.A46_控制台输出load效果)]
+    public class A46_控制台输出load效果 : IVerification
+    {
+        public void Start(string[]? args)
+        {
+            ConsoleSpinner csp = new ConsoleSpinner();
+
+            Console.CursorVisible = false;
+            while(true)
+            {
+                csp.Turn();
+            }
+        }
+    }
+
+    public class ConsoleSpinner
+    {
+        private int counter;
+
+        public void Turn()
+        {
+            this.counter++;
+            switch(this.counter % 4)
+            {
+                case 0:
+                    Console.Write("/");
+                    this.counter = 0;
+                    break;
+
+                case 1:
+                    Console.Write("-");
+                    break;
+
+                case 2:
+                    Console.Write("\\");
+                    break;
+
+                case 3:
+                    Console.Write("|");
+                    break;
+            }
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+    }
+}
--- 技术点与语法/技术点验证/Program.cs
+++ 技术点与语法/技术点验证/Program.cs
@@ -7,7 +7,7 @@ namespace 技术点验证
         public static void Main(string[]? args)
         {
             //开始验证
-            VerificationHelper.StartVerification(VerificationTypeEnum.A45_零宽字符加解密, args);
+            VerificationHelper.StartVerification(VerificationTypeEnum.A46_控制台输出load效果, args);
         }
     }
 }
