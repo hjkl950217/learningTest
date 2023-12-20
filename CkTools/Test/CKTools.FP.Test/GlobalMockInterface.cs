0716ed4dca1b21b4fd9327d1ea4fdb113c1b6a07
hjkl950217 <584880422@qq.com>
2023-12-20 16:57:45 +0800

    抽取公共mock接口

---
+++ CkTools/Test/CKTools.FP.Test/GlobalMockInterface.cs
@@ -0,0 +1,42 @@
+﻿namespace CKTools.FP.Test
+{
+    /// <summary>
+    /// 测试接口
+    /// </summary>
+    public interface IAction
+    {
+        void TestVoid();
+
+        void Test0();
+
+        void Test00();
+
+        void Test1(string a);
+
+        void Test11(int a);
+
+        void Test111(double a);
+
+        void Test2(string b, string a);
+
+        void Test22(string b, int a);
+    }
+
+    /// <summary>
+    /// 测试接口
+    /// </summary>
+    public interface IFunc
+    {
+        int Test0();
+
+        int Test00();
+
+        string Test1(int a);
+
+        int Test11(string a);
+
+        double Test111(double a);
+
+        double Test2(string b, int a);
+    }
+}
