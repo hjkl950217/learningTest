9bd8259ef8c96cbfdc7639dc194a9dee68992e3d
hjkl950217 <584880422@qq.com>
2024-01-20 17:39:36 +0800

    添加UT

--- CkTools/Src/CkTools.FP/Extensions/ObjectExtensions/ObjectExt_Do.cs
+++ CkTools/Src/CkTools.FP/Extensions/ObjectExtensions/ObjectExt_Do.cs
@@ -1,70 +1,73 @@
-﻿//using System.Diagnostics.CodeAnalysis;
-//using CkTools.FP;
+﻿using System.Diagnostics.CodeAnalysis;
+using CkTools.FP;
 
-//namespace System
-//{
-//    /// <summary>
-//    /// 从对象上面直接调用的FP方法
-//    /// </summary>
-//    public static class ObjectExt_Do
-//    {
-//        /// <summary>
-//        /// 对<paramref name="input"/>执行一些操作
-//        /// </summary>
-//        /// <Value>
-//        /// <para><paramref name="input"/>：要处理的值 </para>
-//        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
-//        /// </Value>
-//        /// <typeparam name="TInput">可传递任意类型</typeparam>
-//        /// <returns></returns>
-//        public static TInput Do<TInput>(
-//            this TInput input,
-//            [NotNull] Action<TInput> doFunc)
-//        {
-//            CkFunctions.CheckNullWithException(doFunc);
-//            doFunc(input);
+namespace System
+{
+    /// <summary>
+    /// 从对象上面直接调用的FP方法
+    /// </summary>
+    public static class ObjectExt_Do
+    {
+        /// <summary>
+        /// 对<paramref name="input"/>执行一些操作
+        /// </summary>
+        /// <Value>
+        /// <para><paramref name="input"/>：要处理的值 </para>
+        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
+        /// </Value>
+        /// <typeparam name="TInput">可传递任意类型</typeparam>
+        /// <returns></returns>
+        public static TInput Do<TInput>(
+            this TInput input,
+            [NotNull] Action<TInput> doFunc)
+        {
+            CkFunctions.CheckNullWithException(doFunc);
+            doFunc(input);
 
-//            return input;
-//        }
+            return input;
+        }
 
-//        /// <summary>
-//        /// 对<paramref name="input"/>执行一些操作
-//        /// </summary>
-//        /// <Value>
-//        /// <para><paramref name="input"/>：要处理的值 </para>
-//        /// <para><paramref name="isExecute"/>：判断是否执行，返回true为执行 </para>
-//        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
-//        /// </Value>
-//        /// <typeparam name="TInput">可传递任意类型</typeparam>
-//        /// <returns></returns>
-//        public static TInput DoIf<TInput>(
-//            this TInput input,
-//            [NotNull] Func<TInput, bool> isExecute,
-//            [NotNull] Action<TInput> doFunc)
-//        {
-//            CkFunctions.CheckNullWithException(isExecute, doFunc);
+        /// <summary>
+        /// 对<paramref name="input"/>执行一些操作
+        /// </summary>
+        /// <Value>
+        /// <para><paramref name="input"/>：要处理的值 </para>
+        /// <para><paramref name="isExecute"/>：判断是否执行，返回true为执行 </para>
+        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
+        /// </Value>
+        /// <typeparam name="TInput">可传递任意类型</typeparam>
+        /// <returns></returns>
+        public static TInput DoIf<TInput>(
+            this TInput input,
+            [NotNull] Func<TInput, bool> isExecute,
+            [NotNull] Action<TInput> doFunc)
+        {
+            CkFunctions.CheckNullWithException(isExecute, doFunc);
 
-//            if (isExecute(input)) { doFunc(input); }
+            if(isExecute(input))
+            {
+                doFunc(input);
+            }
 
-//            return input;
-//        }
+            return input;
+        }
 
-//        /// <summary>
-//        /// 对<paramref name="input"/>执行一些操作
-//        /// </summary>
-//        /// <Value>
-//        /// <para><paramref name="input"/>：要处理的值 </para>
-//        /// <para><paramref name="isExecute"/>：判断是否执行，true为执行 </para>
-//        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
-//        /// </Value>
-//        /// <typeparam name="TInput">可传递任意类型</typeparam>
-//        /// <returns></returns>
-//        public static TInput DoIf<TInput>(
-//            this TInput input,
-//            [NotNull] bool isExecute,
-//            [NotNull] Action<TInput> doFunc)
-//        {
-//            return FpObjectDoExtensions.DoIf(input, t => isExecute, doFunc);
-//        }
-//    }
-//}
+        /// <summary>
+        /// 对<paramref name="input"/>执行一些操作
+        /// </summary>
+        /// <Value>
+        /// <para><paramref name="input"/>：要处理的值 </para>
+        /// <para><paramref name="isExecute"/>：判断是否执行，true为执行 </para>
+        /// <para><paramref name="doFunc"/>：将要执行的处理 </para>
+        /// </Value>
+        /// <typeparam name="TInput">可传递任意类型</typeparam>
+        /// <returns></returns>
+        public static TInput DoIf<TInput>(
+            this TInput input,
+            [NotNull] bool isExecute,
+            [NotNull] Action<TInput> doFunc)
+        {
+            return ObjectExt_Do.DoIf(input, t => isExecute, doFunc);
+        }
+    }
+}
---
+++ CkTools/Test/CKTools.FP.Test/Extensions/ObjectExtensions/ObjectExt_DoTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+
+namespace CKTools.FP.Test
+{
+    public class FpObjectDoExtensionTest
+    {
+        [Fact]
+        public void Do_直接执行不报错()
+        {
+            //准备
+            var num = 1;
+
+            //执行
+            10.Do(n => num = n + 1);
+
+            //断言
+            Assert.Equal(11, num);
+        }
+
+        [Fact]
+        public void DoIf_委托判断_直接执行不报错()
+        {
+            //准备
+            var num = 1;
+
+            //执行
+            10.DoIf(t => false, n => num = n + 1);
+
+            //断言
+            Assert.Equal(10, num);
+        }
+
+        [Fact]
+        public void DoIf_直接执行不报错()
+        {
+            //准备
+            var num = 1;
+
+            //执行
+            10.DoIf(false, n => num = n + 1);
+
+            //断言
+            Assert.Equal(10, num);
+        }
+    }
+}
