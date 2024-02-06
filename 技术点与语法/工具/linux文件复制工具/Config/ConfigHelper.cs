03962c5c4acbf16f7c32c60bf8b4074fdf3a4db3
hjkl950217 <584880422@qq.com>
2024-02-06 11:30:18 +0800

    增加默认配置，减少配置文件依赖

--- 技术点与语法/工具/linux文件复制工具/Config/ConfigHelper.cs
+++ 技术点与语法/工具/linux文件复制工具/Config/ConfigHelper.cs
@@ -5,11 +5,22 @@ namespace linux文件复制工具.Config
     public static class ConfigHelper
     {
         public static AppSettings appSettings = null;
+        public static string ConfigPath = "config.json";
 
         public static AppSettings GetConfig()
         {
             if(appSettings == null)
             {
+                // 如果配置文件不存在，则创建默认配置文件
+                if(!File.Exists(ConfigHelper.ConfigPath))
+                {
+                    appSettings=new AppSettings();
+                    appSettings.TryInitDefaultConfig();
+                    string jsonContent = JsonConvert.SerializeObject(appSettings, Formatting.Indented);
+                    File.WriteAllText(ConfigHelper.ConfigPath, jsonContent);
+                    return appSettings;
+                }
+
                 // 读取 JSON 配置文件
                 string json = File.ReadAllText("config.json");
 
