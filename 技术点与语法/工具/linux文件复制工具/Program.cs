0cdfc0a899dc0c28ce48ba574512244130813ebf
长空X <584880422@qq.com>
2024-09-20 21:46:28 +0800

    删除调试代码

--- 技术点与语法/工具/linux文件复制工具/Program.cs
+++ 技术点与语法/工具/linux文件复制工具/Program.cs
@@ -22,8 +22,6 @@ namespace linux文件复制工具
             //args = new[] { "pack", "--db-type=MySql", "-i=intput", "-o=output", "-h=192.168.1.241", "-u=eform", "-p=1qaz2WSX1qaz2WSX", "--port=30001", "" };
             //args = new[] { "pack", "--db-type=Dm", "-i=intput", "-o=output", "-h=192.168.1.241", "-u=eform", "-p=1qaz2WSX1qaz2WSX", "--port=30001", "" };
 
-            args = new[] { "archiveFolder" };
-
             DateTime runDateTime = DateTime.Now;//定义当前时间
             LogHelper.PreLogWriter(runDateTime); //创建日志文件
             LogHelper.WriteLog(Program.separatorMsg); //输出分割符
