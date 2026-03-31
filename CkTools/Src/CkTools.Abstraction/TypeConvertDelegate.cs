4ff2d4b8dec0680cd73934770f67c2e73ccd601f
hjkl950217 <584880422@qq.com>
2026-03-31 16:07:33 +0800

    增加注释说明

--- CkTools/Src/CkTools.Abstraction/TypeConvertDelegate.cs
+++ CkTools/Src/CkTools.Abstraction/TypeConvertDelegate.cs
@@ -1,11 +1,17 @@
 ﻿namespace System
 {
-    public static partial class TypeConvertDelegate
-    {
+/// <summary>
+/// 提供常用的类型转换委托，可直接作为委托使用或组合使用
+/// </summary>
+public static partial class TypeConvertDelegate
+{
         #region 基础转换
 
         #region To Long
 
+        /// <summary>
+        /// 将 <see cref="string"/> 转换为 <see cref="long"/>
+        /// </summary>
         public static Func<string, long> stringToLong = System.Convert.ToInt64;
 
         #endregion To Long
@@ -21,10 +27,25 @@
 
         #region To DateTimeOffset
 
+        /// <summary>
+        /// 将 <see cref="string"/> 解析为 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<string, DateTimeOffset> stringToDateTimeOffset = DateTimeOffset.Parse;
+        /// <summary>
+        /// 将 Unix 时间戳（秒）转换为 UTC 的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<long, DateTimeOffset> longToUtcDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds;
+        /// <summary>
+        /// 将 Unix 时间戳（毫秒）转换为 UTC 的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<long, DateTimeOffset> longToUtcDateTimeOffsetByMilliseconds = DateTimeOffset.FromUnixTimeMilliseconds;
+        /// <summary>
+        /// 将 UTC 时间的 <see cref="DateTimeOffset"/> 转换为本地时间
+        /// </summary>
         public static Func<DateTimeOffset, DateTimeOffset> utcToLocal = t => t.AddOffset(TimeZoneInfo.Local.BaseUtcOffset);
+        /// <summary>
+        /// 将本地时间的 <see cref="DateTimeOffset"/> 转换为 UTC 时间
+        /// </summary>
         public static Func<DateTimeOffset, DateTimeOffset> localToUtc = t => t.AddOffset(-TimeZoneInfo.Local.BaseUtcOffset);
 
         #endregion To DateTimeOffset
@@ -44,13 +65,28 @@
 
         #region 组合转换
 
+        /// <summary>
+        /// 将表示 Unix 时间戳（秒）的 <see cref="string"/> 转换为 UTC 的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<string, DateTimeOffset> longStringToUtcDateTimeOffset = t => longToUtcDateTimeOffset(stringToLong(t));
 
+        /// <summary>
+        /// 将表示 Unix 时间戳（秒）的 <see cref="string"/> 转换为本地时间的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<string, DateTimeOffset> longStringToLocalDateTimeOffset = t => utcToLocal(longToUtcDateTimeOffset(stringToLong(t)));
 
+        /// <summary>
+        /// 将表示 Unix 时间戳（毫秒）的 <see cref="string"/> 转换为 UTC 的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<string, DateTimeOffset> longStringToUtcDateTimeOffsetByMilliseconds = t => longToUtcDateTimeOffsetByMilliseconds(stringToLong(t));
 
+        /// <summary>
+        /// 将表示 Unix 时间戳（毫秒）的 <see cref="string"/> 转换为本地时间的 <see cref="DateTimeOffset"/>
+        /// </summary>
         public static Func<string, DateTimeOffset> longStringToLocalDateTimeOffsetByMilliseconds = t => utcToLocal(longToUtcDateTimeOffsetByMilliseconds(stringToLong(t)));
+        /// <summary>
+        /// 将表示 Unix 时间戳（秒）的 <see cref="string"/> 转换为本地 <see cref="DateTime"/>
+        /// </summary>
         public static Func<string, DateTime> stringToLocalDateTime = t => longToDatetime(stringToLong(t));
 
         #endregion 组合转换
